1d485743189a82d5fda6df230bc3d2d1604bfdf8
Your Name <you@example.com>
2017-01-26 14:02:28 +0000

    performance daily

---
+++ performance_task_daily.bat
@@ -0,0 +1,2 @@
+@echo on
+"C:\Program Files\R\R-3.2.3\bin\R.exe" CMD BATCH C:\Programs\gtc_tasks\performance_task_daily\performance_task_daily.R
---
+++ performance_task_daily_ChangeDate.R
@@ -0,0 +1,51 @@
+# This is the Waiting Time Task R file 
+#======================================
+library(dplyr)
+pacman::p_load(rmarkdown, knitr)
+daysAgo<-10
+## This will need to be changed to your local settings
+
+home_dir <- "C:/Programs/gtc_tasks/performance_task_daily"
+setwd(home_dir)
+
+# Set the directory path for later
+reports_dir <- paste(home_dir,"reports",sep = "/")
+spreadsheets_dir <- paste(home_dir,"spreadsheets",sep = "/")
+
+# Make sure you delete the folders and files from last week 
+unlink("reports", recursive = TRUE, force = FALSE)
+unlink("spreadsheets", recursive = TRUE, force = FALSE)
+
+# Create a directory for spreadsheets otherwise R having a heart attack 
+dir.create("spreadsheets",showWarnings = F)
+
+
+# Go baby run the report
+rmarkdown::render("performance_daily_ChangeDate.Rmd", "html_document", output_dir = "reports")
+
+# Get the list of the generated files 
+html_files <- list.files(reports_dir, recursive=TRUE)
+xlsx_files <- list.files(spreadsheets_dir, recursive=TRUE)
+
+# For each file set create the full path 
+#html_file_paths <- paste(reports_dir, html_files, sep="/")
+#xlsx_files_paths <- paste(spreadsheets_dir, xlsx_files , sep="/")
+xlsx_name <- paste(as.Date(Sys.time() - (daysAgo*60*60*24)), "_", "results.xlsx", sep = '')
+  
+# Create mail lists
+base_list <- "Daria.Alekseeva@greentomatocars.com;Julia.Thomas@transdevplc.co.uk);James.Rowe@greentomatocars.com;sean.sauter@transdevplc.co.uk;Christina.Stone@greentomatocars.com;Ian.Bates@greentomatocars.com;Lee.Holland@greentomatocars.com;controldesk@greentomatocars.com;Duncan.Fendom@greentomatocars.com;maxim.starostin@magenta-technology.com;petr.popov@magenta-technology.com;Paul.Jobling@greentomatocars.com;Tyrone.Hunte@greentomatocars.com;antony.carolan@greentomatocars.com;Haider.Variava@greentomatocars.com;Sophie.Jacobsen@greentomatocars.com"
+#daily_list <- "Daria.Alekseeva@greentomatocars.com"
+
+# Send mail for each client and attach html report + spreadsheet
+library(RDCOMClient)
+
+# Send mail for 3D
+OutApp <- COMCreate("Outlook.Application")
+outMail = OutApp$CreateItem(0)
+outMail[["subject"]] = 'Daily Performance GTC'
+outMail[["To"]] = base_list
+outMail[["body"]] = "Good day. This is an automated e-mail. Daily performance report for previous date is attached. Daria"
+outMail[["Attachments"]]$Add(paste(reports_dir,'performance_daily_ChangeDate.html', sep='/'))
+outMail[["Attachments"]]$Add(paste(spreadsheets_dir,xlsx_name, sep='/'))
+outMail$Send()
+rm(list = c("OutApp","outMail"))
